9fe4a560b7b5338c7e1b4b9d4485837ea9e32e27
Stone Gu <stone_gu@hotmail.com>
2019-04-08 23:08:00 -0400

    token utils

--- src/main/java/com/bizislife/core/utils/TokenUtils.java
+++ src/main/java/com/bizislife/core/utils/TokenUtils.java
@@ -1,8 +1,12 @@
 package com.bizislife.core.utils;
 
+import java.security.Key;
 import java.security.PublicKey;
+import java.util.Date;
 
+import javax.crypto.spec.SecretKeySpec;
 import javax.security.auth.message.AuthException;
+import javax.xml.bind.DatatypeConverter;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.auth.AuthenticationException;
@@ -10,7 +14,9 @@ import org.apache.http.auth.AuthenticationException;
 import com.bizislife.api.graphql.configuration.KeycloakConfig;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 public class TokenUtils {
 	public static Claims parseToken(String token) throws AuthenticationException {
@@ -37,6 +43,14 @@ public class TokenUtils {
 		
 	}
 	
+	public static Claims parseToken4(String jwt, String secretKey) {
+	    //This line will throw an exception if it is not a signed JWS (as expected)
+	    Claims claims = Jwts.parser()
+	            .setSigningKey(DatatypeConverter.parseBase64Binary(secretKey))
+	            .parseClaimsJws(jwt).getBody();
+	    return claims;
+	}	
+	
     public static String getTokenWithoutSignature(String encodedToken) {
         int i = encodedToken.lastIndexOf('.');
         return encodedToken.substring(0, i + 1);
@@ -45,5 +59,36 @@ public class TokenUtils {
     public static String extractBearer(String token) {
         return StringUtils.removeStartIgnoreCase(token, "Bearer ");
     }
+    
+    
+    public static String createJWT(String id, String issuer, String subject, long ttlMillis, String secretKey) {
+    	  
+        //The JWT signature algorithm we will be using to sign the token
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+        long nowMillis = System.currentTimeMillis();
+        Date now = new Date(nowMillis);
+
+        //We will sign our JWT with our ApiKey secret
+        byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(secretKey);
+        Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
+
+        //Let's set the JWT Claims
+        JwtBuilder builder = Jwts.builder().setId(id)
+                .setIssuedAt(now)
+                .setSubject(subject)
+                .setIssuer(issuer)
+                .signWith(signatureAlgorithm, signingKey);
+      
+        //if it has been specified, let's add the expiration
+        if (ttlMillis > 0) {
+            long expMillis = nowMillis + ttlMillis;
+            Date exp = new Date(expMillis);
+            builder.setExpiration(exp);
+        }  
+      
+        //Builds the JWT and serializes it to a compact, URL-safe string
+        return builder.compact();
+    }    
     
 }
