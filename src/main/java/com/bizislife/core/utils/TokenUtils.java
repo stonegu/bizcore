cfb342e5a08688bc15ed96c005d07fb069a07cf2
Stone Gu <stone_gu@hotmail.com>
2019-04-11 23:15:21 -0400

    token utils

--- src/main/java/com/bizislife/core/utils/TokenUtils.java
+++ src/main/java/com/bizislife/core/utils/TokenUtils.java
@@ -1,13 +1,19 @@
 package com.bizislife.core.utils;
 
 import java.security.Key;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
 import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.Date;
 
 import javax.crypto.spec.SecretKeySpec;
 import javax.security.auth.message.AuthException;
 import javax.xml.bind.DatatypeConverter;
 
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.auth.AuthenticationException;
 
@@ -24,14 +30,15 @@ public class TokenUtils {
 		if (token == null) {
 			throw new AuthenticationException("token is missing");
 		}		
-		return Jwts.parser().parseClaimsJws(getTokenWithoutSignature(extractBearer(token))).getBody();
+		return Jwts.parser().parseClaimsJwt(getTokenWithoutSignature(extractBearer(token))).getBody();
 	}
 	
-	public static Claims parseToken2(PublicKey key, String token) throws AuthenticationException {
+	public static Claims parseToken2(String key, String token) throws AuthenticationException, NoSuchAlgorithmException, NoSuchProviderException, InvalidKeySpecException {
 		if (token == null) {
 			throw new AuthenticationException("token is missing");
 		}
-		return Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody();
+		PublicKey pkey = getPublicKey(key, "RSA", "BC");
+		return Jwts.parser().setSigningKey(pkey).parseClaimsJwt(getTokenWithoutSignature(extractBearer(token))).getBody();
 	}
 	
 	public static Claims parseToken3(PublicKey key, String token, KeycloakConfig keycloakConfig) throws AuthenticationException {
@@ -40,7 +47,8 @@ public class TokenUtils {
 		}
 		return Jwts.parser().setSigningKey(key)
 				.requireAudience(keycloakConfig.getResource())
-				.requireIssuer(keycloakConfig.getRealm()).parseClaimsJws(token).getBody();
+				.requireIssuer(keycloakConfig.getRealm())
+				.parseClaimsJwt(getTokenWithoutSignature(extractBearer(token))).getBody();
 		
 	}
 	
@@ -68,6 +76,12 @@ public class TokenUtils {
     public static String extractBearer(String token) {
         return StringUtils.removeStartIgnoreCase(token, "Bearer ");
     }
+    
+    public static PublicKey getPublicKey(String key, String algorithm, String provider) throws NoSuchAlgorithmException, NoSuchProviderException, InvalidKeySpecException {
+        byte[] decodedKey = Base64.decodeBase64(key);
+        X509EncodedKeySpec spec = new X509EncodedKeySpec(decodedKey);
+        KeyFactory kf = KeyFactory.getInstance(algorithm, provider);
+        return kf.generatePublic(spec);    }
     
     
     public static String createJWT(String id, String issuer, String subject, long ttlMillis, String secretKey) {
